2bc294b722cb5d855cf50b80cc43de5c7e7f56e0
rosette1818 <r.umurerwa@alustudent.com>
2026-06-29 03:10:33 -0700

    LEvel 4 tasks

---
+++ quests/quest_18_loop_of_riddles.py
@@ -0,0 +1,9 @@
+secret_number = 7
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Guess the number: "))
+    if guess != secret_number:
+        print("Wrong! Try again.")
+
+print("Correct! You got it!")
